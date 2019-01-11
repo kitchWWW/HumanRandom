625c7a32e5664186a82ecf7d89a60e0828bd3043
Brian Ellis <brian@Brians-MacBook-Pro.local>
2019-01-11 14:53:03 -0600

    random person identification

---
+++ generators/generateBatches.py
@@ -0,0 +1,96 @@
+import random
+import numpy as np
+
+fd = open('../datasets/bothFingers.csv')
+stringLength = 30
+positives = []
+lines = fd.readlines()
+for i in range(len(lines)):
+	sample = lines[i].strip()
+	for z in range(20):
+		halfway = int(len(sample)/2)-stringLength
+		startIndex = random.randint(0,halfway)
+		secondIndex = random.randint(startIndex+stringLength,len(sample)-stringLength)
+		aPortion = sample[startIndex:startIndex+stringLength]
+		bPortion = sample[secondIndex:secondIndex+stringLength]
+		if random.randint(0,1)==0:
+			positives.append((aPortion,bPortion))
+		else:
+			positives.append((bPortion,aPortion))
+
+print positives
+
+
+negatives = []
+
+for i in range(len(lines)):
+	sample = lines[i].strip()
+	for z in range(20):
+		alternativeIndex = i
+		while alternativeIndex == i:
+			alternativeIndex = random.randint(0,len(lines)-1)
+		
+		alternativeSample = lines[alternativeIndex].strip()
+		startIndex = random.randint(0,len(sample)-stringLength)
+		secondIndex = random.randint(0,len(alternativeSample)-stringLength)
+		
+		aPortion = sample[startIndex:startIndex+stringLength]
+		bPortion = alternativeSample[secondIndex:secondIndex+stringLength]
+		if random.randint(0,1)==0:
+			negatives.append((aPortion,bPortion))
+		else:
+			negatives.append((bPortion,aPortion))
+
+print negatives
+
+batchesBad = []
+
+for i in positives:
+	features = list(str(i[0]) + str(i[1]))
+	label = 1
+	batchesBad.append([features,label])
+for i in negatives:
+	features = list(str(i[0]) + str(i[1]))
+	label = 0
+	batchesBad.append([features,label])
+
+batches = []
+
+for b in batchesBad:
+	batches.append((b[0],b[1]))
+
+random.shuffle(batches)
+
+x_train_pre = []
+y_train_pre = []
+
+x_test_pre = []
+y_test_pre = []
+
+splitPoint = len(batches) - int(len(batches)/5)
+
+for b in batches[:splitPoint]:
+	x_train_pre.append(b[0])
+	y_train_pre.append(b[1])
+
+for b in batches[splitPoint:]:
+	x_test_pre.append(b[0])
+	y_test_pre.append(b[1])	
+
+x_train = np.array(x_train_pre,dtype=np.int)
+y_train = np.array(y_train_pre,dtype=np.int)
+
+x_test = np.array(x_test_pre,dtype=np.int)
+y_test = np.array(y_test_pre,dtype=np.int)
+
+print x_train,y_train
+
+
+
+
+
+
+
+
+
+
---
+++ generators/nn.py
@@ -0,0 +1,50 @@
+'''Trains a simple deep NN on the MNIST dataset.
+Gets to 98.40% test accuracy after 20 epochs
+(there is *a lot* of margin for parameter tuning).
+2 seconds per epoch on a K520 GPU.
+'''
+
+from __future__ import print_function
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+from generateBatches import x_train, y_train, x_test, y_test
+
+
+batch_size = 128
+num_classes = 2
+epochs = 20
+
+# the data, split between train and test sets
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(60,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+
+# y_train = keras.to_categorical(y_train)
+
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test)
+                    )
+
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
